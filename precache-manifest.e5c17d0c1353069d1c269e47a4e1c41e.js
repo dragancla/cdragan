c5919bfb832adff05d4bf15bb96af5ce966c7cc0
Claudiu Dragan <dragancla@gmail.com>
2019-04-14 20:56:49 +0200

    publish build

---
+++ precache-manifest.e5c17d0c1353069d1c269e47a4e1c41e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d936b112a52fbe7149a0",
+    "url": "/static/css/main.6e3a1af8.chunk.css"
+  },
+  {
+    "revision": "d936b112a52fbe7149a0",
+    "url": "/static/js/main.d936b112.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49ca803ece93442517e302acb4551fc7",
+    "url": "/index.html"
+  }
+];
