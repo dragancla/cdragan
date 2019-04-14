df7058171755e0fd985bffbfed671cc2a3c9bb6a
Claudiu Dragan <dragancla@gmail.com>
2019-04-14 19:19:51 +0200

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "imagebaseurl":"	",
+    "name": "Claudiu Dragan",
+    "role": "QA Automation Engineer",
+    "linkedinId":"https://www.linkedin.com/in/claudiu-dragan/",
+    "skypeid": "dragancla1992",
+    "roleDescription": " I like dabbling in various parts of development and learn about new technologies or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/claudiu-dragan/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dragancla",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/dragancla",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently working at LeoVegas AB as a QA Automation Engineer. I am a self taught Python and Javascript developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Stockholm, Sweden",
+    "website":"https://dragancla.github.io",
+    "education":[
+      {
+        "UniversityName":"Titu Maiorescu University",
+        "specialization":"Master's Degree in Computer and Information Systems Security/Information Assurance",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2016",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Romanian-American University",
+        "specialization":"Bachelor's Degree in Cybernetics, Statistics and Economic Informatics",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2014",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]	
+  }
+  
+  export default resumeData
