49a4d78134ec7a35d6b1dcb00635b00a878552cd
Claudiu Dragan <dragancla@gmail.com>
2020-06-11 00:10:49 +0200

    update website

---
+++ precache-manifest.bdc3747bba41dfeb052029c27cec2d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e50a2f513e70f818357c8a999ce69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85309c137f1fc7889329",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "32fb4820e3b86d2d97c1",
+    "url": "/static/js/2.35baa03d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35baa03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85309c137f1fc7889329",
+    "url": "/static/js/main.91c15928.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  },
+  {
+    "revision": "8a69e69eee6a01ae6e321a5453a25f52",
+    "url": "/static/media/resume.8a69e69e.pdf"
+  }
+]);
