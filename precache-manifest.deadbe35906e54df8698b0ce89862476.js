b6b33a6ab0005d3959926f5cad6a13009b2e6ded
dragancla <claudiu.dragan@leovegas.com>
2020-07-08 10:25:15 +0000

    Deploying to gh-pages from  @ 0eecb4a71415ea60b6f2053c8af43360d338975c 🚀

---
+++ precache-manifest.deadbe35906e54df8698b0ce89862476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb879c67e189b8b58961fcf1bcc5977c",
+    "url": "/cdragan/index.html"
+  },
+  {
+    "revision": "54383c387f581c71e141",
+    "url": "/cdragan/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "d79a4332572c95512350",
+    "url": "/cdragan/static/js/2.4d6764e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cdragan/static/js/2.4d6764e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54383c387f581c71e141",
+    "url": "/cdragan/static/js/main.41ca6a53.chunk.js"
+  },
+  {
+    "revision": "7e8cff82fad70e4a8829",
+    "url": "/cdragan/static/js/runtime-main.cc65c687.js"
+  },
+  {
+    "revision": "828a16dedad5d8f35243b66721e0b9cd",
+    "url": "/cdragan/static/media/resume.828a16de.pdf"
+  }
+]);
