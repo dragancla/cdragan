2c70ccb2920908ab01dca77d2e972e1dac752580
dragancla <claudiu.dragan@leovegas.com>
2020-07-13 13:25:19 +0000

    Deploying to gh-pages from  @ b6f8d55e4773332f1989d14c89d7984ccbeb526a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdragan/precache-manifest.2b68c74b765d9f7ef48180dfea55e317.js"
+  "/precache-manifest.2cc149b1074d155aa6b165bb7ad2b415.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdragan/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
